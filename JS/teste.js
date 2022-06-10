e1f60706681152536d7f251730122badb1a24561
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-10 13:02:30 -0300

    aaaaaa

---
+++ JS/teste.js
@@ -0,0 +1,6 @@
+var vel = 40.50
+console.log(`a velocidade do seu carro e ${vel}`)
+if (vel > 60) {
+    console.log('voce esta multado por sair da velocidade maxima')
+}
+console.log('usa cinto de seguranca seu burro')
---
+++ JS/teste002.js
@@ -0,0 +1,12 @@
+
+var pais = 'canada'
+
+console.log(`voce e do ${pais}`)
+
+if (pais == 'brasil') {
+    console.log('ola meu amigo')
+}
+
+else {
+    console.log('seu bobao')
+}
