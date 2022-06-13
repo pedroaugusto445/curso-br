5ac00602415a4cdb6704edada8d2b9934542afca
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-13 11:33:21 -0300

    hehehehe

---
+++ JS/ex.001/script.js
@@ -0,0 +1,10 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementByI('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `agora sao ${hora} horas`
+     
+}  
+
