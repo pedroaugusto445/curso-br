e9b61d19442b24923115efad24749201ccf2f4fb
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-13 14:27:37 -0300

    aa

--- JS/ex.001/script.js
+++ JS/ex.001/script.js
@@ -1,10 +1,20 @@
-function carregar(){
+function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementByI('imagem')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-    
-    msg.innerHTML = `agora sao ${hora} horas`
-     
-}  
+    var date = new Date()
+    //var hora = data.getHours() 
+    var hora = 15
+    msg.innerHTML = `agora são ${hora} horas`
+        
+    if (hora >= 0 && hora< 12) {
+        //bom dia
+        img.innerHTML = '<img src="imagems/manha.jpg">'
 
+    } else if (hora >= 12 && hora < 18) {
+        img.innerHTML = '<img src="imagems/tarde.jpg">'
+
+    } else {
+        img.innerHTML = '<img src="imagems/noite.jpg">'
+    } 
+}
---
+++ teste007.js
@@ -0,0 +1,7 @@
+var c = 1
+    do {
+    console.log('tudo bem')
+    c++
+} while (c < 6) {
+    console.log(`pasos ${c}`)
+}
