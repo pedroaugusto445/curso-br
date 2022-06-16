4fadbd7b8833c880669f3518149ff7cdf6b35799
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-16 12:25:50 -0300

    Update script.js

--- desafios/script.js
+++ desafios/script.js
@@ -7,7 +7,24 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passos.value.length === 0) {
         window.alert('seus dados estao errados por favor fasa de novo')
     } else {
-        res.innerHTML += ('')
+        res.innerHTML += 'contando'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passos.value)
+        if (p == 0) {
+            window.alert('conta em valida')
+            p = 1
+        }
+            if (i < f) {
+              for (let c = i ; c <= f ; c += p ){
+                    res.innerHTML += `${c} \u{1F449}`        
+                } 
+            } else {
+                for (let c = i; c >= f ; c -= p ) {
+                    res.innerHTML += `${c} \u{1F449}`
+                }
+            }
+
     }
 
 }
