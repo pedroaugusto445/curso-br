a8f094607a653db3c5d2a4a9910a219f8cd143b1
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-17 15:23:42 -0300

    Update script.js

--- desafios/desafios002/script.js
+++ desafios/desafios002/script.js
@@ -1,3 +1,20 @@
 function tabuada() {
-    
+    let nu = window.document.getElementById('txtn')
+    let tab = window.document.getElementById('seltab')
+
+    if (nu.value.length == 0) {
+        window.alert('seus dados estao errados escreva a tabuada ')
+    } else {
+        let n = Number(nu.value)  
+        for (let c ;c = n; c <= tab) {
+            let item = window.document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c }`
+            tab.appendChild(item)
+            c++
+        }
+
+    }
+
+
+
 }
