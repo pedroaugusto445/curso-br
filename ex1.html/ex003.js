5736a1a88c8ebefa0066bd61936c660a862fcaeb
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-21 10:58:42 -0300

    Update ex003.js

--- ex1.html/ex003.js
+++ ex1.html/ex003.js
@@ -1,3 +1,10 @@
 let num = [6,2,1,9,7,8,10]
+/*
+for(let pos = 0 ;pos < num.length ;pos++) {
+    console.log(`a posicao do vector ${pos} e o numero e ${num[pos]}`)
+}
+*/
 
-for(let pos =0 ;pos<=;)
+for (let pos in num) {
+    console.log(`a posicao ${pos} e vector e ${num[pos]}`)
+}
