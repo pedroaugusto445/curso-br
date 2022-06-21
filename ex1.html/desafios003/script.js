15f8d37836d74a4c9f97f3a7709be620360d65f4
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-21 13:25:12 -0300

    que

---
+++ ex1.html/desafios003/script.js
@@ -0,0 +1,9 @@
+let num = window.document.getElementById('dd')
+let list = window.document.getElementById('sele')
+let res = window.document.getElementById('res')
+let d = []
+function guardar() {
+        if (num > 100 || num < 0) {
+            window.alert('seus dados estao incorretos')
+        }
+}
---
+++ ex1.html/ex004.js
@@ -0,0 +1,9 @@
+function parmpi(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+console.log(parmpi(223))
