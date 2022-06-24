f02ad87e442a1bba341b4c3e4f50eca506a5768d
pedroaugusto445 <magodoscodicos@gmail.com>
2022-06-24 12:48:47 -0300

    ex

--- ex1.html/desafios003/script.js
+++ ex1.html/desafios003/script.js
@@ -1,10 +1,10 @@
-let num = window.document.getElementById('dd')
-let list = window.document.getElementById('sele')
-let res = window.document.getElementById('res')
-let d = []
+let num = document.getElementById('num')
+let fli = document.getElementById('list')
+let rr  = document.getElementById('res')
+let valores =  []
 
-function inlista(n) {
-    if (Number(n) >= 1 && Number(n) <= 100 ) {
+function inlist(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
@@ -12,17 +12,18 @@ function inlista(n) {
 }
 
 function isnumero(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true  
+    if (l.indexOf(Number(n)) != 1) {
+        return true
     } else {
         return false
     }
 }
 
 function guardar() {
-    if (isnumero(num.value) && !inlista(num.value, valores)) {
+    if (isnumero(num.value) && !inlist(num.value, valores)) {
         window.alert('tudo ok')
+
     } else {
-        alert('seus dados estao errados')
+        alert('seus dados estao errados ou voce nao deixou alguma coisa em branco')
     }
 }
